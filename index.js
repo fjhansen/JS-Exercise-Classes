41b8ed9a76c3d9bcf0ebddf469bb1192a87e6a8e
Frances Hansen <francesjuniperhansen@gmail.com>
2020-01-31 01:59:46 -0800

    final for mvp

--- index.js
+++ index.js
@@ -80,7 +80,26 @@ class Person {
 */
 
 class Car {
-
+   constructor(model,milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+   }
+   fill(gallons) {
+    this.tank += gallons;
+   }
+   drive(distance) {
+     let mpg = distance / this.milesPerGallon;
+     if (mpg <= this.tank) {
+       this.odometer = this.odometer + distance;
+       this.tank = this.tank - distance / this.milesPerGallon;
+     } else {
+       this.odometer = this.odometer + this.tank * this.milesPerGallon;
+       this.tank = 0;
+       return `I ran out of fuel at ${this.odometer}!`;
+     }
+   };
 }
 
 /*
